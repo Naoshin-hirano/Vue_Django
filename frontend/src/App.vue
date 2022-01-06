3ec7efb5adfaa9fb53d98fb06acd0f8b79d15449
Hirano <hnaoya1120@gmail.com>
2022-01-07 01:56:37 +0900

    djangoRFとvueとのCRUD実装

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,24 @@
 <template>
-  <v-app>
-    <router-view />
-  </v-app>
+    <v-app>
+        <v-app-bar color="success" dark app>
+            <v-toolbar-title>求人情報</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn text to="/">ホーム</v-btn>
+                <v-btn text to="/add">仕事追加</v-btn>
+            </v-toolbar-items>
+        </v-app-bar>
+        <v-main>
+            <router-view />
+        </v-main>
+        <v-footer color="success" dark app class="justify-center">
+            Django Startup - Step3
+        </v-footer>
+    </v-app>
 </template>
+
+<script>
+export default {
+
+};
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import List from "../views/List.vue";
+import Detail from "../views/Detail.vue";
+import Add from "../views/Add.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "list",
+    component: List
   },
+  {
+    path: "/detail/:id",
+    name: "detail",
+    component: Detail,
+    props: true
+  },
+  {
+    path: "/add",
+    name: "add",
+    component: Add
+  }
 ];
 
 const router = new VueRouter({
